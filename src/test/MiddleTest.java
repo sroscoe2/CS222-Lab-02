2e8f23931d33f4e7e8ed28482d5f237e26bca893
sroscoe2 <51931272+sroscoe2@users.noreply.github.com>
2025-02-26 15:42:36 -0800

    cmp two arrays equal

--- src/test/MiddleTest.java
+++ src/test/MiddleTest.java
@@ -7,19 +7,30 @@ import main.Middle;
 
 class MiddleTest {
 
+	boolean checkArraysEqual(char[] one, char[] two){
+		if (one.length != two.length)
+			return false;
+		for(int i=0; i<one.length; i++) {
+			if (one[i] != two[i])
+				return false;
+		}
+		return true;
+	}
+
 	@Test
 	void middleTest01() {
 		char[] expected = {'h','e','l','l','o','w','o','r','l','d'};
 		char[] actual = Middle.splitToLetters("hello", "world");
-		Assertions.assertEquals(expected,actual);
+		Assertions.assertTrue(checkArraysEqual(expected,actual));
 	}
 	
 	@Test
 	void middleTest02() {
 		char[] expected = {'h','i','h','o','w','d','o','y','o','u','l','i','k','e','t','h','e','c','l','a','s','s'};
-		char[] actual = Middle.splitToLetters("hi", "how do you like the class");
-		Assertions.assertEquals(expected,actual);
+		char[] actual = Middle.splitToLetters("hi", "howdoyouliketheclass");
+		Assertions.assertTrue(checkArraysEqual(expected,actual));
 	}
+
 
 
 }
